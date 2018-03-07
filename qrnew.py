3ce693aa93b3ab99438bc82ccf71e8f3c4303507
jclasul <janclaes04@icloud.com>
2018-03-08 00:03:59 +0100

    Google Forms V3

--- qrnew.py
+++ qrnew.py
@@ -24,21 +24,24 @@ for itemstock in stock.itertuples():
     Merk = str(itemstock.Merk)   
     IDnr = str(itemstock.IDnr)
     VerkoopPrijs = str(itemstock.VerkoopPrijs) 
-    
+
+    # Google Forms URLs
     url_start = "https://docs.google.com/forms/d/e/1FAIpQLSfxJE74YdD7KoYasdWXEa1VFpTONJf79L3juT2goonwyhSzZA/viewform?usp=pp_url&entry.406303456&entry.184234757="
     url_mid = "&entry.1724678309="
     url_mid2 = "&entry.113696914="
     url_end = "&entry.72186599&entry.77984989&entry.516085731"
 
+    # Get all sizes and quantities to generate pre-filled Google Form URL QRs
     maten = stock.loc[:,stock.columns.str.contains("Maat")].dropna(axis=1)   
 
     for maat in maten.columns:
         for n in range(maten.loc[:,maat][0]):
             # create link to google form
-            full_url_list = [url_start, IDnr, url_mid, VerkoopPrijs, url_mid2, maat, url_end]
+            maat_string = maat.split("[")[-1].strip("[]")
+            full_url_list = [url_start, IDnr, url_mid, VerkoopPrijs, url_mid2, maat_string, url_end]
             full_url = "".join(full_url_list)
 
-            QRCODE = qrcode.make(URL).convert("L").resize((290,290))
+            QRCODE = qrcode.make(full_url).convert("L").resize((290,290))
             imgs = [IMG12[0],IMG12[1],QRCODE]        
             imgs_comb = PIL.Image.fromarray(np.hstack([i for i in imgs]))
 
@@ -47,12 +50,11 @@ for itemstock in stock.itertuples():
 
             # Draw text
             draw.text((355,160), " ".join(["€",VerkoopPrijs]), fill=0, font=fnt_large)
-            draw.text((460,100), maat, fill=0, font=fnt_medium)
-            draw.text((360,100), "Maat :", fill=0, font=fnt_medium)
+            draw.text((360,100), maat, fill=0, font=fnt_medium)
             draw.text((700,270), IDnr, fill=0, font=fnt_small)
 
             # Combine text and image
-            IMG_NAME = 'QRcodes/' + Merk + "_" + IDnr + "_" + str(n+1) + ".PNG"
+            IMG_NAME = 'QRcodes/' + Merk + "_" + IDnr + "_" + maat + "_" + str(n+1) + ".PNG"
             imgs_comb.save(IMG_NAME)
     
     
