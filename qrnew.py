93a4fddc918ad06d104d7df7f6f143dd8af28592
jclasul <janclaes04@icloud.com>
2018-03-07 23:47:50 +0100

    Update Google Docs V2

--- qrnew.py
+++ qrnew.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 import qrcode
 
 stock = pd.read_csv("C:/Users/JCLA/Downloads/DATABASE_IN - Form responses 1.csv")
-stock = stock[stock["LABELED"]!=True]
+# stock = stock[stock["LABELED"]!=True]
 
 # Convert and Format DATABASE
 stock["Timestamp"] = pd.to_datetime(stock.Timestamp)
@@ -20,30 +20,50 @@ fnt_small = ImageFont.truetype('C:/Windows/Fonts/Arial.ttf',size=15)
 fnt_medium = ImageFont.truetype('C:/Windows/Fonts/Arial.ttf',size=30)
 fnt_large = ImageFont.truetype('C:/Windows/Fonts/Arial.ttf',size=50)
 
-for itemstock in stock.itertuples():    
-    MAAT = str(itemstock.Maat)
-    MERK = str(itemstock.Merk)
-    PRIJS = "€" + str(itemstock.VerkoopPrijs)
-    ARTICLE_NR = str(itemstock.LeverancierReferentie)
-    ONZE_REF = str(itemstock.OnzeReferentie)
+for itemstock in stock.itertuples():
+    Merk = str(itemstock.Merk)   
+    IDnr = str(itemstock.IDnr)
+    VerkoopPrijs = str(itemstock.VerkoopPrijs) 
+    
+    url_start = "https://docs.google.com/forms/d/e/1FAIpQLSfxJE74YdD7KoYasdWXEa1VFpTONJf79L3juT2goonwyhSzZA/viewform?usp=pp_url&entry.406303456&entry.184234757="
+    url_mid = "&entry.1724678309="
+    url_mid2 = "&entry.113696914="
+    url_end = "&entry.72186599&entry.77984989&entry.516085731"
 
-    QR = {'Maat':MAAT,'Merk':MERK,'Prijs':PRIJS,'Article_nr':ARTICLE_NR,'Reference_nr':ONZE_REF}
-    QRCODE = qrcode.make(QR).convert("L").resize((290,290))
-    imgs = [IMG12[0],IMG12[1],QRCODE]        
-    imgs_comb = PIL.Image.fromarray(np.hstack([i for i in imgs]))
+    maten = stock.loc[:,stock.columns.str.contains("Maat")].dropna(axis=1)   
 
-    # Get drawing context
-    draw = ImageDraw.Draw(imgs_comb)
-
-    # Draw text
-    draw.text((355,160), PRIJS, fill=0, font=fnt_large)
-    draw.text((460,100), MAAT, fill=0, font=fnt_medium)
-    draw.text((360,100), "Maat :", fill=0, font=fnt_medium)
-    draw.text((700,270), ONZE_REF, fill=0, font=fnt_small)
-           
     for maat in maten.columns:
         for n in range(maten.loc[:,maat][0]):
-            print(maat, n)
+            # create link to google form
+            full_url_list = [url_start, IDnr, url_mid, VerkoopPrijs, url_mid2, maat, url_end]
+            full_url = "".join(full_url_list)
+
+            QRCODE = qrcode.make(URL).convert("L").resize((290,290))
+            imgs = [IMG12[0],IMG12[1],QRCODE]        
+            imgs_comb = PIL.Image.fromarray(np.hstack([i for i in imgs]))
+
+            # Get drawing context
+            draw = ImageDraw.Draw(imgs_comb)
+
+            # Draw text
+            draw.text((355,160), " ".join(["€",VerkoopPrijs]), fill=0, font=fnt_large)
+            draw.text((460,100), maat, fill=0, font=fnt_medium)
+            draw.text((360,100), "Maat :", fill=0, font=fnt_medium)
+            draw.text((700,270), IDnr, fill=0, font=fnt_small)
+
             # Combine text and image
-            IMG_NAME = 'QRcodes/' + MERK + "_" + ONZE_REF + "_" + str(j+1) + ".PNG"
+            IMG_NAME = 'QRcodes/' + Merk + "_" + IDnr + "_" + str(n+1) + ".PNG"
             imgs_comb.save(IMG_NAME)
+    
+    
+    
+    
+
+
+
+    
+
+
+
+
+
